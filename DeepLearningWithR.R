481b06be80b2ba47e69c156e836a563775cea79a
Amanda Plunkett <rstudio@ip-172-31-85-217.ec2.internal>
2018-12-16 19:12:35 +0000

    Chapter 3 additions (through page 69).

--- DeepLearningWithR.R
+++ DeepLearningWithR.R
@@ -32,3 +32,86 @@ network %>% fit(train_images, train_labels, epochs=5, batch_size=128)
 (metrics <- network %>% evaluate(test_images, test_labels))
 
 network %>% predict_classes(test_images[1:10,])
+
+
+######################################################################
+# Chapter 3
+
+library(keras)
+
+imdb <- dataset_imdb(num_words = 10000) #ibdb is a list of 2 lists (each of length 2)
+c(c(train_data, train_labels), c(test_data, test_labels)) %<-% imdb # %<-% is the multi-assignment operator from the zeallot package.
+str(train_data[[1]])
+
+#Need to turn list of numbers into tensors. We'll use a binary vector: 1=word appears in review, 0 otherwise
+vectorize_sequence <- function(sequences, dimension = 10000){
+  results <- matrix(0, nrow = length(sequences), ncol = dimension)
+  for (i in 1:length(sequences)){
+    results[i,sequences[[i]]] <- 1
+  results  
+  }
+}
+
+x_train <- vectorize_sequence(train_data)
+x_test <- vectorize_sequence(test_data)
+
+str(x_train[1,])
+
+#Convert labels from integer to numeric:
+y_train <- as.numeric(train_labels)
+y_test <- as.numeric(test_labels)
+
+#Define the model:
+model <- keras_model_sequential() %>%
+  layer_dense(units = 16, activation = "relu", input_shape = c(10000)) %>%
+  layer_dense(units = 16, activation = "relu") %>%
+  layer_dense(units = 1, activation = "sigmoid")
+
+#Compile the model:
+model %>% compile(
+  optimizer = "rmsprop",
+  loss = "binary_crossentropy",
+  metrics = c("accuracy")
+)
+
+#Validation:
+val_indices <- 1:10000
+x_val <- x_train[val_indices,]
+partial_x_train <- x_train[-val_indices,]
+y_val <- y_train[val_indices]
+partial_y_train <- y_train[-val_indices]
+
+#Train model
+history <- model %>% fit(
+  partial_x_train,
+  partial_y_train,
+  epochs = 20,
+  batch_size = 512,
+  validation_data = list(x_val,y_val)
+)
+
+#See results:
+str(history)
+plot(history) #uses ggplot2 if it's available, otherwise base plotting.
+#We can see here that after the 4th epoc, we seem to have overfitting.
+
+#Retrain with only 4 epochs:
+history <- model %>% fit(
+  partial_x_train,
+  partial_y_train,
+  epochs = 4,
+  batch_size = 512
+)
+results <- model %>% evaluate(x_test,y_test)
+results
+
+#This is a naive approach that obtains ~85% accuracy. With state-of-the art approaches, 95% accuracy should be attainable.
+
+#Use trained network to generate predictions on new data:
+model %>% predict(x_test[1:10,])
+
+#TODO: spin up a gpu machine on AWS to run above code. 
+ 
+
+
+
