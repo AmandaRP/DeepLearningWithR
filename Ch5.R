8c08f1875d9ac35bef3f5d14b6ac7cb60887f6c6
AmandaRP <amanda@dekleineorchards.com>
2019-02-11 16:23:15 +0000

    Added code through listing 5.15

--- Ch5.R
+++ Ch5.R
@@ -49,7 +49,8 @@ results <- model %>% evaluate(test_images, test_labels)
 
 ####### 5.2 Dogs vs Cats example
 
-original_dataset_dir <- "~/DeepLearningWithR/data/catsVsDogsData"
+#Set up directory structure and copy files.
+original_dataset_dir <- "~/DeepLearningWithR/data/catsVsDogsData/train"
 
 base_dir <- "~/DeepLearningWithR/data/cats_and_dogs_small"
 dir.create(base_dir)
@@ -103,5 +104,137 @@ fnames <- paste0("dog.", 1501:2000, ".jpg")
 file.copy(file.path(original_dataset_dir, fnames),
           file.path(test_dogs_dir))
 
-#TODO: unzip data in catsVsDogsData folder. Run commands above to move files.
-#book page 123
+#Listing 5.5 Instantiating a small convnet for dogs vs cats classificaiton
+library(keras)
+
+model <- keras_model_sequential() %>%
+  layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = "relu",
+                input_shape = c(150, 150, 3)) %>%
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_conv_2d(filters = 64, kernel_size = c(3,3), activation = "relu") %>%
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_conv_2d(filters = 128, kernel_size = c(3,3), activation = "relu") %>%
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_conv_2d(filters = 128, kernel_size = c(3,3), activation = "relu") %>%
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_flatten() %>%
+  layer_dense(units = 512, activation = "relu") %>%
+  layer_dense(units = 1, activation = "sigmoid")
+
+#compile model
+model %>% compile(
+  loss = "binary_crossentropy",
+  optimizer = optimizer_rmsprop(lr = 1e-4),
+  metrics = c("acc")
+)
+
+#Listing 5.7: Using image_data_generator to read images from directories
+
+#rescale all images by 1/255
+train_datagen <- image_data_generator(rescale = 1/255)
+validation_datagen <- image_data_generator(rescale = 1/255)
+
+train_generator <- flow_images_from_directory(
+  train_dir,
+  train_datagen,
+  target_size = c(150,150),  #resize all images to 150x150
+  batch_size = 20,
+  class_mode = "binary"
+)
+
+validation_generator <- flow_images_from_directory(
+  validation_dir,
+  validation_datagen,
+  target_size = c(150,150),  #resize all images to 150x150
+  batch_size = 20,
+  class_mode = "binary"
+)
+
+#Listing 5.8: Fitting the model using a batch generator
+history <- model %>% fit_generator(
+  train_generator,
+  steps_per_epoch = 100,
+  epochs = 30,
+  validation_data = validation_generator,
+  validation_steps = 50
+)
+
+#save the model:
+model %>% save_model_hdf5("cats_and_dogs_small_1.h5")
+
+plot(history) #we see overfitting is an issue.
+
+#Listing 5.11: Setting up a data augmentation configuration
+datgen <- image_data_generator(
+  rescale = 1/255,
+  rotation_range = 40,
+  width_shift_range = 0.2,
+  height_shift_range = 0.2,
+  shear_range = 0.2,
+  zoom_range = 0.2,
+  horizontal_flip = TRUE,
+  fill_mode = "nearest"
+)
+
+#See listing 5.12 for viewing randonly augmented images.
+
+model <- keras_model_sequential() %>%
+  layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = "relu",
+                input_shape = c(150, 150, 3)) %>%
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_conv_2d(filters = 64, kernel_size = c(3,3), activation = "relu") %>%
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_conv_2d(filters = 128, kernel_size = c(3,3), activation = "relu") %>%
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_conv_2d(filters = 128, kernel_size = c(3,3), activation = "relu") %>%
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_flatten() %>%
+  layer_dropout(rate = 0.5) %>%       #added this to combat correlation and overfitting
+  layer_dense(units = 512, activation = "relu") %>%
+  layer_dense(units = 1, activation = "sigmoid")
+
+model %>% compile(
+  loss = "binary_crossentropy",
+  optimizer = optimizer_rmsprop(lr = 1e-4),
+  metrics = c("acc")
+)
+
+#Listing 5.14: Training the convnet using data-augmentation generators
+
+datagen <- image_data_generator(
+  rescale = 1/255,
+  rotation_range = 40,
+  width_shift_range = 0.2,
+  height_shift_range = 0.2,
+  shear_range = 0.2,
+  zoom_range = 0.2,
+  horizontal_flip = TRUE
+)
+
+test_datagen <- image_data_generator(rescale = 1/255)
+
+train_generator <- flow_images_from_directory( #validation data shouldn't be augmented
+  train_dir,
+  datagen,
+  target_size = c(150, 150),
+  batch_size = 32,
+  class_mode = "binary"
+)
+
+validation_generator <- flow_images_from_directory(
+  validation_dir,
+  test_datagen,
+  target_size = c(150,150),
+  batch_size = 32,
+  class_mode = "binary"
+)
+
+history <- model %>% fit_generator(
+  train_generator,
+  steps_per_epoch = 100,
+  epochs = 100,
+  validation_data = validation_generator,
+  validation_steps = 50
+)
+
+model %>% save_model_hdf5("cats_and_dogs_small_2.h5")
