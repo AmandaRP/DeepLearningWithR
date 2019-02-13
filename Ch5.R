2408474696430dfd893957f4cddb424147062389
AmandaRP <amanda@dekleineorchards.com>
2019-02-13 04:03:08 +0000

    added more ch 5 material

--- Ch5.R
+++ Ch5.R
@@ -238,3 +238,167 @@ history <- model %>% fit_generator(
 )
 
 model %>% save_model_hdf5("cats_and_dogs_small_2.h5")
+
+#Listing 5.16: Instantiating the VGG16 convolutional base
+library(keras)
+
+conv_base <- application_vgg16(
+  weights = "imagenet",
+  include_top = FALSE,
+  input_shape = c(150,150,3)
+)
+
+#Listing 5.17: Extracting features using the pretrained convolutional base
+base_dir <- "/home/rstudio-user/DeepLearningWithR/data/cats_and_dogs_small"
+train_dir <- file.path(base_dir,"train")
+validation_dir <- file.path(base_dir,"validation")
+test_dir <- file.path(base_dir,"test")
+
+datagen <- image_data_generator(rescale = 1/255)
+batch_size <- 20
+
+extract_features <- function(directory, sample_count){
+  
+  features <- array(0, dim = c(sample_count, 4, 4, 512))
+  labels <- array(0, dim = c(sample_count))
+  
+  generator <- flow_images_from_directory(
+    directory = directory,
+    generator = datagen,
+    target_size = c(150,150),
+    batch_size = batch_size,
+    class_mode = "binary"
+  )
+  
+  i <- 0
+  while(TRUE){
+    batch <- generator_next(generator)
+    inputs_batch <- batch[[1]]
+    labels_batch <- batch[[2]]
+    features_batch <- conv_base %>% predict(inputs_batch)
+    
+    index_range <- ((i*batch_size)+1):((i+1) * batch_size)
+    features[index_range,,,] <- features_batch
+    labels[index_range] <- labels_batch
+    
+    i <- i+1
+    if(i * batch_size >= sample_count)
+      break
+  }
+  
+  list(
+    features = features,
+    labels = labels
+  )
+} 
+train <- extract_features(train_dir, 2000)
+validation <- extract_features(validation_dir, 1000)
+test <- extract_features(test_dir, 1000)
+
+reshape_features <- function(features){
+  array_reshape(features,dim = c(nrow(features), 4*4*512))
+}
+train$features <- reshape_features(train$features)
+validation$features <- reshape_features(validation$features)
+test$features <- reshape_features(test$features)
+
+model <- keras_model_sequential() %>%
+  layer_dense(units = 256, activation = "relu",
+              input_shape = 4*4*512) %>%
+  layer_dropout(rate = 0.5) %>%
+  layer_dense(units = 1, activation = "sigmoid")
+
+model %>% compile(
+  optimizer = optimizer_rmsprop(lr = 2e-5),
+  loss = "binary_crossentropy",
+  metrics = c("accuracy")
+)
+
+history <- model %>% fit(
+  train$features, train$labels,
+  epochs = 30,
+  batch_size = 20,
+  validation_data = list(validation$features, validation$labels)
+)
+
+#Listing 5.20: Adding a densely connected classifier on top of the convolutional base
+model <- keras_model_sequential() %>%
+  conv_base() %>%
+  layer_flatten() %>%
+  layer_dense(units = 256, activation = "relu") %>%
+  layer_dense(units = 1, activation = "sigmoid")
+
+freeze_weights(conv_base) #do this so that the weights aren't updated.
+
+#Listing 5.21 Training the model end to end with a frozen convolutional base.
+train_datagen <- image_data_generator(
+  rescale = 1/255,
+  rotation_range = 40,
+  width_shift_range = 0.2,
+  height_shift_range = 0.2,
+  shear_range = 0.2,
+  zoom_range = 0.2,
+  horizontal_flip = TRUE,
+  fill_mode = "nearest"
+)
+
+test_datagen <- image_data_generator(rescale = 1/255)
+
+train_generator <- flow_images_from_directory(
+  train_dir,
+  train_datagen,
+  target_size = c(150,150),
+  batch_size = 20,
+  class_mode = "binary"
+)
+
+validation_generator <- flow_images_from_directory(
+  validation_dir,
+  test_datagen,
+  target_size = c(150,150),
+  batch_size = 20,
+  class_mode = "binary"
+)
+
+model %>% compile(
+  loss = "binary_crossentropy",
+  optimizer = optimizer_rmsprop(lr = 2e-5),
+  metrics = c("accuracy")
+  
+)
+
+history <- model %>% fit_generator(
+  train_generator,
+  steps_per_epoch = 100,
+  epochs = 30,
+  validation_data = validation_generator,
+  validation_steps = 50
+)
+
+#Listing 5.22: Unfreezing previously frozen layers
+unfreeze_weights(conv_base, from = "block3_conv1")
+
+#Listing 5.23: Fine-tuning the model
+model %>% compile(
+  loss = "binary_crossentropy",
+  optimizer = optimizer_rmsprop(lr = 1e-5),
+  metrics = c("accuracy")
+)
+
+history <- model %>% fit_generator(
+  train_generator, 
+  steps_per_epoch = 100,
+  epochs = 10,
+  validation_data = validation_generator,
+  validation_steps = 50
+)
+
+test_generator <- flow_images_from_directory(
+  test_dir,
+  test_datagen,
+  target_size = c(150,150),
+  batch_size = 20,
+  class_mode = "binary"
+)
+
+model %>% evaluate_generator(test_generator, steps = 50)
